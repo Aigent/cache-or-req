ee38fc980e3474ef07d83710c9fcd01ff43ec41d
Sebastian Lagemann <sebastian@aigent.co>
2018-11-09 13:02:50 +0100

    added lock for same requests to avoid that the same request is fetched more than once at a time

--- CacheOrReq.js
+++ CacheOrReq.js
@@ -3,8 +3,8 @@ const request = require('request-promise');
 class CacheOrReq {
     constructor(cacheTtl, cacheNotFound) {
         this.cacheTtl = cacheTtl || 1000;
-        this.cacheNotFound = cacheNotFound;
         this.cache = {};
+        this.locks = {};
         this.callbacks = {
             fetchSuccess: [],
             fetchError: [],
@@ -13,6 +13,18 @@ class CacheOrReq {
             hit: []
         };
     }
+
+    /**
+     * adds a callback to the list which gets called if an event happens, the following types are possible:
+     * - fetchSuccess
+     * - fecthError
+     * - update
+     * - miss
+     * - hit
+     *
+     * @param type
+     * @param cb
+     */
     addCallback(type, cb) {
         if(this.callbacks[type]) {
             this.callbacks[type].push(cb);
@@ -20,6 +32,13 @@ class CacheOrReq {
             throw new Error('invalid callback type ' + type);
         }
     }
+
+    /**
+     * triggers an event and calls corresponding configured callbakcs if there are any
+     *
+     * @param type
+     * @param data
+     */
     triggerEvent(type, data) {
         if(this.callbacks[type] && this.callbacks[type].length > 0) {
             for(let cb of this.callbacks[type]) {
@@ -27,6 +46,13 @@ class CacheOrReq {
             }
         }
     }
+
+    /**
+     * returns the local cache version by given cache id
+     *
+     * @param id
+     * @returns {*}
+     */
     getCache(id) {
         if(this.cache[id]) {
             return this.cache[id];
@@ -34,6 +60,14 @@ class CacheOrReq {
             return false;
         }
     }
+
+    /**
+     * sets cache with request or error response
+     *
+     * @param id
+     * @param data
+     * @param error
+     */
     setCache(id, data, error) {
         this.cache[id] = {
             lastUpdate: new Date(),
@@ -41,9 +75,56 @@ class CacheOrReq {
             error: error
         };
     }
+
+    /**
+     * locks a given id and returns true if lock was successful
+     *
+     * @param id
+     * @returns {boolean}
+     */
+    lock(id) {
+        if(!this.locks[id]) {
+            this.locks[id] = new Date();
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * unlocks a given id and returns true if it was locked
+     *
+     * @param id
+     * @return {boolean}
+     */
+    unlock(id) {
+        if(this.locks[id]) {
+            this.locks[id] = false;
+            return true;
+        }
+        return false
+    }
+
+    /**
+     * clears complete cache and locks
+     *
+     */
     clearCache() {
         this.cache = {};
+        this.locks = {};
     }
+
+    /**
+     * returns data by given request options in the following manner:
+     * - if the first time called the promise will resolved after the data was fetched
+     * - all calls after that with the same request options will resolve with a previously fetched and in the object instance stored version
+     * - if the previous version is outdated, determined by the cacheTtl configuration value, it will start fetching the data based on the
+     *   request options and as soon as the request is finished all calls to get will return the new version
+     *
+     * This behaviour allows to have a fast and liable service even though if underlaying services are not available and/or slow
+     *
+     * @param requestOptions
+     * @returns {Promise<any>}
+     */
     get(requestOptions) {
         const self = this;
         const requestOptionsSerialised = JSON.stringify(requestOptions);
@@ -71,24 +152,40 @@ class CacheOrReq {
             }
         });
     }
+
+    /**
+     * fetchs the data based on the request options. It ensures that the same request is just fetched once at the same time.
+     *
+     * @todo add awareness of remote hosts and throttling reuqests to avoid DoS behaviour as soon as a underlaying service is up after downtime
+     *
+     * @param requestId
+     * @param requestOptions
+     * @returns {Promise<any>}
+     */
     fetch(requestId, requestOptions) {
         const self = this;
         return new Promise((resolve, reject) => {
-            request(requestOptions).then((response) => {
-                self.setCache(requestId, response, false);
+            if(self.lock(requestId)) {
+                request(requestOptions).then((response) => {
+                    self.setCache(requestId, response, false);
+                    resolve();
+                    self.triggerEvent('fetchSuccess', {response: response, requestId: requestId});
+                    self.unlock(requestId);
+                }).catch((error) => {
+                    let lastCacheValue = self.getCache(requestId);
+                    self.setCache(
+                        requestId,
+                        // if the remote api response with an array but we had already data we shouldn't overwrite it
+                        (lastCacheValue && lastCacheValue.content ? lastCacheValue.content : false),
+                        error
+                    );
+                    resolve();
+                    self.triggerEvent('fetchError', {error: error, requestId: requestId});
+                    self.unlock(requestId);
+                });
+            } else {
                 resolve();
-                self.triggerEvent('fetchSuccess', {response: response, requestId: requestId});
-            }).catch((error) => {
-                let lastCacheValue = self.getCache(requestId);
-                self.setCache(
-                    requestId,
-                    // if the remote api response with an array but we had already data we shouldn't overwrite it
-                    (lastCacheValue && lastCacheValue.content?lastCacheValue.content:false),
-                    error
-                );
-                resolve();
-                self.triggerEvent('fetchError', {error: error, requestId: requestId});
-            });
+            }
         });
     }
 }
